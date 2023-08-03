1c3cc9d70340d053000336696d14a5a8b9951db8
Sadman Yasir Rafat <rafatsh800@gmail.com>
2023-08-03 21:22:36 +0600

    Fixed Stylings

--- src/Components/UtilityButton.jsx
+++ src/Components/UtilityButton.jsx
@@ -9,7 +9,7 @@ import MembershipList from './MembershipList';
 const UtilityButton = () => {
     return (
         <div className='px-5 py-3'>
-            <div className='flex bg-red-500 text-white justify-between border-4 border-white rounded-2xl items-center px-5 py-3  shadow-lg mb-5'>
+            <div className='flex bg-red-500 text-white justify-between border-4 border-white rounded-2xl items-center px-5 py-3 transition-all duration-500 hover:scale-105 shadow-lg mb-5'>
                 <a href="#">
                     <h1 className='text-3xl underline font-semibold'>Download App</h1>
                 </a>
@@ -17,13 +17,13 @@ const UtilityButton = () => {
                     <img className='w-full h-full' src={App} alt="" />
                 </div>
             </div>
-            <div className='flex bg-blue-500 text-white justify-between border-4 border-white rounded-2xl items-center px-5 py-3 shadow-lg'>
+            <div className='flex bg-blue-500 text-white justify-between border-4 border-white rounded-2xl items-center px-5 py-3 transition-all duration-500 hover:scale-105 shadow-lg'>
                 <a href="#">
                     <div className='flex justify-center items-center'>
                         <div className='h-10 w-10 me-2'>
                             <img className='w-full h-full' src={I} alt="" />
                         </div>
-                        <h1 className='text-3xl underline font-semibold'>Company Profile</h1>
+                        <h1 className='text-3xl font-semibold'>Company Profile</h1>
                     </div>
                 </a>
                 <div className='h-14 w-14'>
