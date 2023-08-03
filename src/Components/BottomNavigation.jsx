86d74e64ec33c853b1bab03ed84753c75810c730
Sadman Yasir Rafat <rafatsh800@gmail.com>
2023-08-03 20:05:57 +0600

    Fixed Functionality

--- src/Components/BottomNavigation.jsx
+++ src/Components/BottomNavigation.jsx
@@ -1,5 +1,7 @@
 /* eslint-disable no-unused-vars */
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+
+
 import Home from '../assets/Images/home.png';
 import Mission from '../assets/Images/trophy.png';
 import Team from "../assets/Images/group.png";
@@ -9,10 +11,24 @@ import User from "../assets/Images/user.png";
 const BottomNavigation = () => {
 
     const [clickedLink, setClickedLink] = useState(null);
+    const componentRef = useRef(null);
 
     const handleClick = (index) => {
         setClickedLink(index === clickedLink ? null : index);
     };
+    useEffect(() => {
+        const handleOutsideClick = (e) => {
+            if (componentRef.current && !componentRef.current.contains(e.target)) {
+                setClickedLink(null);
+            }
+        };
+
+        document.addEventListener('click', handleOutsideClick);
+        return () => {
+            document.removeEventListener('click', handleOutsideClick);
+        };
+    }, []);
+
 
     const linksData = [
         { text: 'Home', imgSrc: Home },
@@ -26,16 +42,16 @@ const BottomNavigation = () => {
 
     return (
         <div className="fixed-bottom-div ">
-            <div className="bottom-navigation fixed bottom-0 w-1/2 flex justify-around items-center bg-[#fee2e2] shadow-md py-3 rounded-lg">
+            <div ref={componentRef} className="bottom-navigation fixed bottom-0 w-1/2 flex justify-around items-center bg-[#fee2e2] shadow-md py-1 rounded-lg">
 
                 {linksData.map((link, index) => (
                     <a
                         key={index}
-                        className={`flex flex-col items-center text-gray-600 hover:text-black transition-all duration-300 ${clickedLink === index ? 'scale-105 -mt-10' : ''
+                        className={`flex flex-col items-center text-gray-600 hover:text-black transition-all duration-300 ${clickedLink === index ? 'scale-105 -mt-5' : 'scale-100 -mt-0'
                             }`}
                         onClick={() => handleClick(index)}
                     >
-                        <div className='h-10 w-10'>
+                        <div className='h-10 w-10 p-2 rounded-full bg-[#fee2e2]'>
                             <img className='w-full h-full' src={link.imgSrc} alt={link.text} />
                         </div>
                         <p className="font-semibold text-black text-sm mb-1">{link.text}</p>
